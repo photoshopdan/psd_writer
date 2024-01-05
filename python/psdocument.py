a5e96f2e3e5316b0f90f1ed747219fb7d57e5e8b
Dan Kemp <kempdan7@gmail.com>
2024-01-05 11:01:27 +0000

    Add license

--- python/psdocument.py
+++ python/psdocument.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2024 Dan Kemp. All rights reserved.
+# This source code is licensed under the MIT license found in the 
+# LICENSE file in the root directory of this source tree.
+
 import ctypes
 
 lib = ctypes.cdll.LoadLibrary(
